051f3cacde83b91d10984b5eb9ae174eff6f1d8f
Daniel Sanu <sanudaniel46@gmail.com>
2025-06-24 23:10:13 -1000

    added contact button to heropage

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 // import rectangle from "./public/Rectangle.png";
 import Danny from "./public/Danny.png";
+import facebook from "./public/facebook.png";
+import x from "./public/x.png";
+import linkedin from "./public/linkedin.png";
+import instagram from "./public/instagram.png";
+import github from "./public/github.png";
+
 
 export default function Hero() {
   return (
     <section className="hero">
       {/* hero image */}
-      <div className="flex flex-col py-30">
+      <div className="flex flex-col py-20">
         {/* <img src={rectangle} alt="" /> */}
         <div className="mx-auto">
           <img src={Danny} alt="" className="shadow-2xl shadow-white" />
@@ -23,6 +29,16 @@ export default function Hero() {
             ipsum facere quibusdam dignissimos eaque cum possimus amet, et
             impedit dicta adipisci. Debitis, sequi
           </p>
+        <div className="flex gap-7 pt-[40px]">
+          <img src={facebook} alt="" />
+          <img src={x} alt="" />
+          <img src={linkedin} alt="" />
+          <img src={instagram} alt="" />
+          <img src={github} alt="" />
+        </div>
+          <div className="rounded-full bg-[#02BDEA] w-[180px] p-3 mt-10">
+            <p className="text-[16px] text-white ">Contact Me</p>
+          </div>
         </div>
       </div>
     </section>
