03d446227c51605860586f98aff8c4902a943669
Daniel Sanu <sanudaniel46@gmail.com>
2025-06-25 23:21:05 -1000

    worked with responsivenes

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -3,15 +3,15 @@ import React from "react";
 export default function About() {
   return (
     <section className="bg-zinc-50 items-center flex flex-col pt-10  ">
-      <h1 className="text-[35px] font-semibold sm:text-[50px] md:text-[60px]">
+      <h1 className="text-[35px] font-semibold sm:text-[40px] md:text-[45px]">
         About <span className="text-[#02BDEA]">Me</span>
       </h1>
-      <div className="mt-7 w-[332px] rounded-2xl bg-white flex flex-col items-center  p-10  shadow-2xl shadow-sky-200  sm:w-[500px] md:w-[570px] lg:w-[690px] xl:w-[1024px] 2xl:w-[1440px]">
-        <p className="text-[18px] w-[282px] mt-5 font-semibold text-center sm:text-[18px] sm:w-[450px] md:text-[20 px] lg:w-[500px] lg:text-[24px] lg:w-[500px] lg:text-[24px]  ">
+      <div className="mt-7 w-[332px] rounded-2xl bg-white flex flex-col items-center  p-10  shadow-2xl shadow-sky-200  sm:w-[500px] md:w-[670px] lg:w-[750px] xl:w-[1024px] 2xl:w-[1140px]">
+        <p className="text-[18px] w-[282px] mt-5 font-semibold text-center sm:text-[18px] sm:w-[450px] md:text-[20 px] lg:w-[700px] lg:text-[22px] xl:w-[900px] xl:text-[24px] 2xl:w-[900px] 2xl:text-[24px]  ">
           I am a front-end developer based in Nigeria. Has Mechanical
           Engineering background.
         </p>
-        <p className="text-center text-[16px] w-[260px] mt-5 leading-7">
+        <p className="text-center text-[16px] w-[260px] mt-5 leading-7 sm:text-[16px] sm:w-[400px] md:text-[16px] md:w-[600px] md:leading-9 lg:w-[650px] lg:text-[16px] lg:leading-10 xl:w-[900px] xl:text-[18px] xl:leading-12  2xl:w-[900px] 2xl:text-[20px]">
           I am a front-end developer based in Sydney looking for exciting
           opportunities. Has Mechanical Engineering background. Likes to focus
           on accessibility when developing. Passionate and curious about solving
@@ -21,9 +21,11 @@ export default function About() {
         </p>
 
         <div className="flex flex-col items-center gap-5 text-[16px]">
-          <h1 className="text-[30px] font-semibold mt-10">My Skills</h1>
-          <div className="flex gap-3">
-            <p className="bg-sky-50 rounded-lg py-1 px-2 ">HTML</p>
+          <h1 className=" font-semibold mt-10 text-[18px] w-[282px]  text-center sm:text-[18px] sm:w-[450px] md:text-[20px] lg:w-[700px] lg:text-[30px] xl:w-[900px] xl:text-[32px] 2xl:w-[900px] 2xl:text-[34px] ">
+            My Skills
+          </h1>
+          <div className="flex gap-3 ">
+            <p className="bg-sky-50 rounded-lg py-1 px-2">HTML</p>
             <p className="bg-sky-50 rounded-lg py-1 px-2">CSS</p>
             <p className="bg-sky-50 rounded-lg py-1 px-2">Javascript</p>
           </div>
@@ -35,7 +37,7 @@ export default function About() {
         </div>
         <div className="flex flex-col items-center">
           <h1 className="text-[30px] font-semibold mt-10">My Experience</h1>
-          <p className="text-center text-[16px] w-[260px] mt-5 leading-7">
+          <p className="text-center text-[16px] w-[260px] mt-5 leading-7 sm:text-[16px] sm:w-[400px] md:text-[16px] md:w-[600px] md:leading-9 lg:w-[650px] lg:text-[16px] lg:leading-10 xl:w-[900px] xl:text-[18px] xl:leading-12  2xl:w-[900px] 2xl:text-[20px] ">
             I am a front-end developer based in Sydney looking for exciting
             opportunities. Has Mechanical Engineering background. Likes to focus
             on accessibility when developing. Passionate and curious about
--- src/Components/TopServices.jsx
+++ src/Components/TopServices.jsx
@@ -6,51 +6,75 @@ import code from "./public/code.png";
 export default function TopServices() {
   return (
     <section className="bg-zinc-100 mt-10 py-20 flex flex-col items-center h-[1669px]">
-      <h1 className="text-[35px] font-semibold lg:text-[80px]">
+      <h1 className="text-[35px] font-semibold sm:text-[40px] md:text-[45px]">
         Top <span className="text-[#02BDEA]">Services</span>
       </h1>
-      <div className="mt-7 w-[332px] bg-white h-[446px] mx-auto flex justify-center py-10 rounded-2xl">
-        <div className="w-[270px] flex flex-col items-center gap-5 leading-7">
-          <div>
-            <img src={web} alt="" />
+      <div className="lg:grid lg:grid-cols-2 2xl:grid 2xl:grid-cols-3 gap-10">
+        <div className="mt-7 w-[332px] bg-white h-[446px] mx-auto flex justify-center py-10 rounded-2xl ">
+          <div className="w-[270px] flex flex-col items-center gap-5 leading-7">
+            <div>
+              <img src={web} alt="" />
+            </div>
+            <h1 className="font-semibold text-2xl text-center">
+              Responsive Web Design
+            </h1>
+            <p className="text-center">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor
+              provident exercitationem quae. Vero ea id explicabo debitis rerum
+              alias, inventore molestias aliquam reprehenderit. Iure deleniti
+              vel iusto, voluptatem ut.
+            </p>
           </div>
-          <h1 className="font-semibold text-2xl text-center">Responsive Web Design</h1>
-          <p className="text-center">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor
-            provident exercitationem quae. Vero ea id explicabo debitis rerum
-            alias, inventore molestias aliquam reprehenderit. Iure deleniti vel
-            iusto, voluptatem ut.
-          </p>
         </div>
-      </div>
-      <div className="mt-7 w-[332px] bg-white h-[446px] mx-auto flex justify-center py-10 rounded-2xl">
-        <div className="w-[270px] flex flex-col items-center gap-5 leading-7">
-          <div>
-            <img src={mobile} alt="" />
+        <div className="mt-7 w-[332px] bg-white h-[446px] mx-auto flex justify-center py-10 rounded-2xl">
+          <div className="w-[270px] flex flex-col items-center gap-5 leading-7">
+            <div>
+              <img src={mobile} alt="" />
+            </div>
+            <h1 className="font-semibold text-2xl text-center">
+              Mobile App Design
+            </h1>
+            <p className="text-center">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor
+              provident exercitationem quae. Vero ea id explicabo debitis rerum
+              alias, inventore molestias aliquam reprehenderit. Iure deleniti
+              vel iusto, voluptatem ut.
+            </p>
           </div>
-          <h1 className="font-semibold text-2xl text-center">Mobile App Design</h1>
-          <p className="text-center">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor
-            provident exercitationem quae. Vero ea id explicabo debitis rerum
-            alias, inventore molestias aliquam reprehenderit. Iure deleniti vel
-            iusto, voluptatem ut.
-          </p>
         </div>
-      </div>
-      <div className="mt-7 w-[332px] bg-white h-[446px] mx-auto flex justify-center py-10 rounded-2xl">
-        <div className="w-[270px] flex flex-col items-center gap-5 leading-7">
-          <div>
-            <img src={code} alt="" />
+        <div className="mt-7 w-[332px] bg-white h-[446px] mx-auto flex justify-center py-10 rounded-2xl">
+          <div className="w-[270px] flex flex-col items-center gap-5 leading-7">
+            <div>
+              <img src={code} alt="" />
+            </div>
+            <h1 className="font-semibold text-2xl text-center">
+              Web (Front-end) Development
+            </h1>
+            <p className="text-center">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor
+              provident exercitationem quae. Vero ea id explicabo debitis rerum
+              alias, inventore molestias aliquam reprehenderit. Iure deleniti
+              vel iusto, voluptatem ut.
+            </p>
           </div>
-          <h1 className="font-semibold text-2xl text-center">Web (Front-end) Development</h1>
-          <p className="text-center">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor
-            provident exercitationem quae. Vero ea id explicabo debitis rerum
-            alias, inventore molestias aliquam reprehenderit. Iure deleniti vel
-            iusto, voluptatem ut.
-          </p>
         </div>
-      </div>
+        <div className="mt-7 w-[332px] bg-white h-[446px] mx-auto flex justify-center py-10 rounded-2xl">
+          <div className="w-[270px] flex flex-col items-center gap-5 leading-7">
+            <div>
+              <img src={code} alt="" />
+            </div>
+            <h1 className="font-semibold text-2xl text-center">
+              Application (Front-end) Development
+            </h1>
+            <p className="text-center">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor
+              provident exercitationem quae. Vero ea id explicabo debitis rerum
+              alias, inventore molestias aliquam reprehenderit. Iure deleniti
+              vel iusto, voluptatem ut.
+            </p>
+          </div>
+        </div>
+      </div>  
     </section>
   );
 }
