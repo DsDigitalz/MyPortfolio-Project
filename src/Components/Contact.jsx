3d80871852f929471a8d88d01bcf520b29be70a8
Daniel Sanu <sanudaniel46@gmail.com>
2025-06-30 17:04:49 -1000

    included a drop-down at the header

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -38,14 +38,17 @@ export default function Contact() {
               className="border-b rounded border-zinc-300 outline-sky-200 px-3 py-1"
             />
             <label htmlFor="">Message </label>
-            <input
-              type="text"
-              name="message"
-              id="message"
+            <textarea
+              name=""
+              id=""
+              cols="30"
+              rows="3"
               className="border-b rounded border-zinc-300 outline-sky-200 px-3 py-1"
-            />
+            ></textarea>
           </form>
-          <div className="bg-[#02BDEA] p-3 text-white font-semibold flex justify-center rounded-full w-[50%] mt-10">Submit</div>
+          <div className="bg-[#02BDEA] p-3 text-white font-semibold flex justify-center rounded-full w-[50%] mt-10 cursor-pointer active:bg-[#50cbea]">
+            Submit
+          </div>
         </div>
       </div>
     </section>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-import hamburger from "./public/hamburger.png";
+// import hamburger from "./public/hamburger.png";
 
 export default function Navbar() {
   return (
     <section>
       {/* <div className="lg:flex justify-between px-10 py-5"> */}
-        {/* <div className="font-bold">DSDIGITALZ</div> */}
-        {/* <div className="flex justify-between items-center p-5"> */}
-        {/* <ul className="flex gap-[55px] text-[16px] font-medium pr-20">
+      {/* <div className="font-bold">DSDIGITALZ</div> */}
+      {/* <div className="flex justify-between items-center p-5"> */}
+      {/* <ul className="flex gap-[55px] text-[16px] font-medium pr-20">
           <li className="cursor-pointer hover:text-sky-400  active:text-sky-500">
             Home
           </li>
@@ -26,13 +26,24 @@ export default function Navbar() {
         </ul> */}
       {/* </div> */}
 
-      <div className="flex justify-between px-10 py-5">
+      <div className="flex justify-between px-10 py-5 items-center">
         <div className="font-bold">DSDIGITALZ</div>
         <div className="flex justify-between items-center p-5"></div>
-        <div>
-          <img src={hamburger} alt="" />
+
+        <div className="flex flex-col gap-1 justify-center relative">
+          <div className="bg-black rounded px-4 py-0.5"></div>
+          <div className="bg-black rounded px-4 py-0.5"></div>
+          <div className="bg-black rounded px-4 py-0.5"></div>
+          {/* <img src={hamburger} alt="" /> */}
         </div>
       </div>
+          <div className="absolute right-5 top-15 font-semibold bg-white rounded-2xl px-10 py-6 shadow-2xl leading-10">
+            <p className="cursor-pointer hover:text-sky-400  active:text-sky-500">Home</p>
+            <p className="cursor-pointer hover:text-sky-400  active:text-sky-500">About</p>
+            <p className="cursor-pointer hover:text-sky-400  active:text-sky-500">Services</p>
+            <p className="cursor-pointer hover:text-sky-400  active:text-sky-500">Portfolio</p>
+            <p className="cursor-pointer hover:text-sky-400  active:text-sky-500">Contact</p>
+          </div>
     </section>
   );
 }
