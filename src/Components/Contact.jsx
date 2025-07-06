dce05ab037c77b1701ebf6c77cbc4df7ce5eb3da
Daniel Sanu <sanudaniel46@gmail.com>
2025-07-06 02:29:57 -1000

    modified the Contact component

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -11,14 +11,17 @@ import { FaGithub } from "react-icons/fa";
 import { BsSend } from "react-icons/bs";
 export default function Contact() {
   return (
-    <section className="w-100% pt-10  flex flex-col justify-center bg-white relative lg:flex lg:flex-1" id="contact">
+    <section
+      className="w-100% pt-10  flex flex-col justify-center bg-white relative lg:flex lg:flex-1"
+      id="contact"
+    >
       <div className="mt-10">
         <div className="">
           <h1 className="text-[35px] font-semibold sm:text-[40px] md:text-[45px] text-center">
             Contact <span className="text-sky-300">Me</span>
           </h1>
-          <div className=" bg-white shadow-2xl p-10 px-20 shadow-sky-200 lg:flex justify-center gap-[100px] lg:mt-10">
-            <div>
+          <div className=" bg-white shadow-2xl p-10 lg:px-30 shadow-sky-200 lg:flex lg:justify-center gap-[100px] lg:mt-10">
+            <div className="lg:w-[50%] xl:w-[50%] 2xl:w-[40%] ">
               <div className="lg:w-[70%]">
                 <h1 className="font-semibold text-2xl mt-5 mb-5 lg:mt-0">
                   Lets's Connect
@@ -60,7 +63,7 @@ export default function Contact() {
               </div>
             </div>
 
-            <form className="mt-15 flex flex-col border-zinc-400 gap-3 sm:w-[50%] md:[50%] lg:w-[50%] lg:mt-0 xl:w-[30%] 2xl:w-[20%] ">
+            <form className="mt-15 flex flex-col border-zinc-400 gap-3 sm:w-[50%] md:w-[70%] lg:w-[50%] lg:mt-0 xl:w-[30%] 2xl:w-[20%] ">
               <label htmlFor="">
                 Name <span className="text-sky-300">*</span>
               </label>
@@ -96,7 +99,7 @@ export default function Contact() {
               ></textarea>
               <div className="bg-[#02BDEA] p-3 text-white font-semibold gap-3 flex justify-center rounded-full w-[50%] mt-10 cursor-pointer active:bg-[#50cbea]">
                 Submit
-                <BsSend className="text-[24px] bg-sky-300 rounded-full p-1"/>
+                <BsSend className="text-[24px] bg-sky-300 rounded-full p-1" />
               </div>
             </form>
           </div>
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,32 +4,32 @@ import ScrollToTop from "./ScrollToTop";
 
 export default function Footer() {
   return (
-    <section className="bg-black p-15 w-full mt-10" id="footer">
-      <div className="flex justify-center gap-30 items-center lg:flex lg:justify-between lg:px-20 lg:py-10 ">
+    <section className="bg-black p-10 w-full mt-10" id="footer">
+      <div className="flex flex-col justify-center gap-10  items-center lg:flex-row lg:justify-between lg:px-20 lg:py-10 ">
         <div className="font-bold text-[20px] lg:text-[24px] text-white">
           DS<span className="text-[#02BDEA]">DIGIT</span>
           <span className="text-white">ALZ</span>
         </div>
-        <div className="text-white flex flex-col gap-5 lg:flex lg:flex-row">
-          <p className="cursor-pointer hover:text-sky-400  active:text-sky-500">
-            Home
-          </p>
-          <p className="cursor-pointer hover:text-sky-400  active:text-sky-500">
-            About
-          </p>
-          <p className="cursor-pointer hover:text-sky-400  active:text-sky-500">
-            Services
-          </p>
-          <p className="cursor-pointer hover:text-sky-400  active:text-sky-500">
-            Portfolio
-          </p>
-          <p className="cursor-pointer hover:text-sky-400  active:text-sky-500">
-            Contact
-          </p>
-        </div>
+        <ul className="text-zinc-300 text-[12px] flex items-center gap-5 lg:flex lg:flex-row">
+          <li className="cursor-pointer hover:text-sky-400  active:text-sky-500">
+            <a href="#home"> Home</a>
+          </li>
+          <li className="cursor-pointer hover:text-sky-400  active:text-sky-500">
+            <a href="#about">About</a>
+          </li>
+          <li className="cursor-pointer hover:text-sky-400  active:text-sky-500">
+            <a href="#home">Services</a>
+          </li>
+          <li className="cursor-pointer hover:text-sky-400  active:text-sky-500">
+            <a href="#portfolio"> Portfolio</a>
+          </li>
+          <li className="cursor-pointer hover:text-sky-400  active:text-sky-500">
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
       </div>
-      <div className="text-center mt-20">
-        <h1 className="text-zinc-400">
+      <div className="text-center mt-10 border-t-1 pt-5 text-zinc-400 w-[85%]  sm:w-[50%] lg:w-[85%] xl:w-[90%] mx-auto md">
+        <h1 className="text-zinc-400 text-[12px] md:text-[14px] lg:text-[14px]">
           ©2025DSDigitalzPortfolio.All rights reserved.
         </h1>
       </div>
