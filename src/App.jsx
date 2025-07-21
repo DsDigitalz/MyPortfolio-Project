cb5631803b720483c5e4f2f0c0fe05e7e96212d6
Daniel Sanu <sanudaniel46@gmail.com>
2025-07-20 23:33:47 -1000

    commented Testimonials.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ export default function App() {
       <About />
       <TopServices />
       <Portfolio />
-      <Testimonials />
+      {/* <Testimonials /> */}
       <Contact />
       <Footer/>
       <ScrollToTop/>
