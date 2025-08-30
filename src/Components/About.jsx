a151ebf6152930ef1988e64f03d38b87c7f1a954
Daniel Sanu <sanudaniel46@gmail.com>
2025-08-29 14:38:16 -1000

    change bg of About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -29,7 +29,7 @@ export default function About() {
   return (
     <section
       className={`flex flex-col items-center pt-20 pb-20 px-4 ${
-        isDarkMode ? "bg-slate-950 text-white" : "bg-white text-black"
+        isDarkMode ? "bg-slate-950 text-white" : "bg-zinc-50 text-black"
       }`}
       id="about"
     >
